fd3e012776f26ffb88d2e7af33b6d27f4a2b6dc9
Noppawit Hansompob <noppawit.han@gmail.com>
2019-03-03 16:54:25 -0800

    Add Search feature with Core Data functionality

--- HourTech/ViewControllers/SearchResultsViewController.swift
+++ HourTech/ViewControllers/SearchResultsViewController.swift
@@ -7,22 +7,38 @@
 //
 
 import UIKit
+import CoreData
 
 class SearchResultsViewController: UIViewController {
     
-    var techkyProfiles = TechkyProfile.createProfile()
+//    var techkyProfiles = TechkyProfile.createProfile()
+    var techkyProfiles = [Techky_Profile]()
 
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var additionalNavigationBackground: UILabel!
     @IBOutlet weak var searchTableView: UITableView!
     
+    @IBOutlet weak var searchStatusLabel: UILabel!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchBar.delegate = self
+        
         self.dismissKeyboardOnTap()
         
 //        searchTableView.rowHeight = UITableView.automaticDimension
 //        searchTableView.estimatedRowHeight = 500
-
+        
+        
+        //MARK - Uncomment hardcodeProfile for the first time run after finished come back and comment it again since it is hardcoding.
+//        hardcodeProfile()
+        
+        loadSearchResult()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,6 +50,77 @@ class SearchResultsViewController: UIViewController {
         if segue.identifier == "techky_profile_segue" {
             let dvc = segue.destination as! TechkyProfileViewController
             dvc.profile = techkyProfiles[searchTableView.indexPathForSelectedRow!.row]
+        }
+    }
+    
+    func hardcodeProfile() {
+        
+        let newProfile1 = Techky_Profile(context: context)
+        newProfile1.firstname = "Maggie"
+        newProfile1.lastname = "Vu"
+        newProfile1.title = "Full-stack Developer"
+        newProfile1.profile_description = "Highly motivated bla bla bla bla"
+        techkyProfiles.append(newProfile1)
+        
+        let newProfile2 = Techky_Profile(context: context)
+        newProfile2.firstname = "Diego"
+        newProfile2.lastname = "Rodrigues De Oliveira"
+        newProfile2.title = "Front-end Developer"
+        newProfile2.profile_description = "Skillful, hard-working. 2-year student at Langara. Available for design, presentation, logo design..."
+        techkyProfiles.append(newProfile2)
+        
+        let newProfile3 = Techky_Profile(context: context)
+        newProfile3.firstname = "Noppawit"
+        newProfile3.lastname = "Hansompob"
+        newProfile3.title = "Back-end Developer"
+        newProfile3.profile_description = "Skillful, hard-working. 2-year student at Langara. Available for design, presentation, logo design..."
+        techkyProfiles.append(newProfile3)
+        
+        let newProfile4 = Techky_Profile(context: context)
+        newProfile4.firstname = "Andra"
+        newProfile4.lastname = "Iskandar"
+        newProfile4.title = "Quality Assurance Developer"
+        newProfile4.profile_description = "Skillful, hard-working. 2-year student at Langara. Available for design, presentation, logo design..."
+        techkyProfiles.append(newProfile4)
+        
+        let newProfile5 = Techky_Profile(context: context)
+        newProfile5.firstname = "Julia"
+        newProfile5.lastname = "Stanovsky"
+        newProfile5.title = "Designer"
+        newProfile5.profile_description = "Skillful, hard-working. 2-year student at Langara. Available for design, presentation, logo design..."
+        techkyProfiles.append(newProfile5)
+        
+        saveProfile()
+    }
+    
+    func saveProfile() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        searchTableView.reloadData()
+    }
+    
+    func loadSearchResult(with request: NSFetchRequest<Techky_Profile> = Techky_Profile.fetchRequest()) {
+        do {
+            techkyProfiles = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+        updateStatusLabel(count: techkyProfiles.count)
+        
+        searchTableView.reloadData()
+    }
+    
+    func updateStatusLabel(count: Int) {
+        if count == 0 {
+            searchStatusLabel.text = "No result found for \(searchBar.text!)"
+        } else if count < 2 {
+            searchStatusLabel.text = "\(count) result found for \(searchBar.text!)"
+        } else {
+            searchStatusLabel.text = "\(count) results found for \"\(searchBar.text!)\""
         }
     }
 
@@ -63,4 +150,40 @@ extension SearchResultsViewController: UITableViewDelegate, UITableViewDataSourc
         performSegue(withIdentifier: "techky_profile_segue", sender: self)
     }
     
+}
+
+extension SearchResultsViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Techky_Profile> = Techky_Profile.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadSearchResult(with: request)
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadSearchResult()
+            
+            searchStatusLabel.text = ""
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        } else {
+            
+            let request : NSFetchRequest<Techky_Profile> = Techky_Profile.fetchRequest()
+            
+            request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+            
+            request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+            
+            loadSearchResult(with: request)
+            
+        }
+    }
 }
